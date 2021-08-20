0d48d6aadf161549b14693cc22dd8df1e3995ac9
Abrar26 <56127835+Abrar26@users.noreply.github.com>
2021-08-20 11:25:06 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <p>h3lppd</p>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+  document.getElementById("root")
+);
